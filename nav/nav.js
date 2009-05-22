d4579d58c48cf74b5549361a4b3b13f30d664f6a
jiz <jksonc@gmail.com>
2009-05-22 11:28:49 +0800

    profiling

--- nav/nav.js
+++ nav/nav.js
@@ -59,7 +59,7 @@ function create_menu(basepath)
 			'<li><a href="'+base+'general/routing.html">URI Routing</a></li>' +
 			'<li><a href="'+base+'general/errors.html">錯誤處理</a></li>' +
 			'<li><a href="'+base+'general/caching.html">快取</a></li>' +
-			'<li><a href="'+base+'general/profiling.html">Profiling Your Application</a></li>' +
+			'<li><a href="'+base+'general/profiling.html">Application 效能分析</a></li>' +
 			'<li><a href="'+base+'general/managing_apps.html">管理 Applications</a></li>' +
 			'<li><a href="'+base+'general/alternative_php.html">變換 PHP 句法</a></li>' +
 			'<li><a href="'+base+'general/security.html">安全性</a></li>' +
